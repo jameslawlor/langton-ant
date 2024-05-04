30860e2256995ae7fcc124cc006bf221e6efb769
James Lawlor <jameslawlor1987@gmail.com>
2024-05-04 15:43:12 +0300

    convert to yml and paths in input handling

--- src/pyturmite/utils/input_handling.py
+++ src/pyturmite/utils/input_handling.py
@@ -1,22 +1,16 @@
 import argparse
-from pyturmite.constants import (
-    CANVAS_SIZE,
-    N_STEPS,
-    PLOT_MODE,
-    RULESET,
-)
+import yaml
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-
-    parser.add_argument("--n_steps", required=False, type=int, default=N_STEPS)
-
-    parser.add_argument("--canvas_size", required=False, type=int, default=CANVAS_SIZE)
-
-    parser.add_argument("--plot_mode", required=False, type=str, default=PLOT_MODE)
-
-    parser.add_argument("--ruleset", required=False, type=str, default=RULESET)
-
+    parser.add_argument("--ant_config_path", required=True, type=str)
+    parser.add_argument("--plotting_config_path", required=True, type=str)
     args = parser.parse_args()
     return args
+
+
+def load_yaml_file_to_json(path):
+    with open(path, "r") as stream:
+        return yaml.safe_load(stream)
+
